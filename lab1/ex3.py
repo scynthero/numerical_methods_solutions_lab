7818726d1eba456629fd512e2498c836ae665a77
Filip Masiukiewicz <scynthero@gmail.com>
2019-03-01 23:56:06 +0100

    Fix a use of deprecated string formatting

--- lab1/ex3.py
+++ lab1/ex3.py
@@ -1,3 +1,3 @@
 def minimal(array = [1,15,23,-8, -12, 56]):
     return min(array), array.index(min(array))
-print("Minimal value is: %.2f and it's index is: %d" % minimal())
+print("Minimal value is: {0[0]} and it's index is: {0[1]}".format(minimal()))
