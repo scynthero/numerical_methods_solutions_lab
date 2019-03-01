ff33bc7a21d0a57ddd2a14c842cebf82e0eb481e
Filip Masiukiewicz <scynthero@gmail.com>
2019-03-02 00:26:00 +0100

    Funny solution

--- lab1/ex4.py
+++ lab1/ex4.py
@@ -3,5 +3,5 @@ import numpy
 import matplotlib.pyplot
 
 if len(argv) != 1:
-    matplotlib.pyplot.plot([0, argv[1]], [0, 0])
+    matplotlib.pyplot.plot([0, argv[1]], [0, 0]) # x and y
 matplotlib.pyplot.show()
