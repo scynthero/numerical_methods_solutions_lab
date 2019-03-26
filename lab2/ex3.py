7e58b9cfcb5eeb272b6bb98bdec4a543af654875
Filip Masiukiewicz <scynthero@gmail.com>
2019-03-27 00:01:21 +0100

    Fix for negative numbers

--- lab2/ex3.py
+++ lab2/ex3.py
@@ -2,4 +2,7 @@ import cs50
 
 x = cs50.get_int("x: ")
 y = cs50.get_int("y: ")
-print("X is divisible by Y" if x % y == 0 else "X is not divisible by Y")
+if y == 0:
+    print("can't divide by zero")
+else:
+    print("X is divisible by Y" if x % y == 0 else "X is not divisible by Y")
--- lab2/ex4.py
+++ lab2/ex4.py
@@ -2,4 +2,7 @@ import cs50
 
 x = cs50.get_float("x: ")
 y = cs50.get_float("y: ")
-print("X is divisible by Y" if round(x, 2) % round(y, 2) == 0 else "X is not divisible by Y")
+if y == 0:
+    print("can't divide by zero")
+else:
+    print("X is divisible by Y" if round(x, 2) % round(y, 2) == 0 else "X is not divisible by Y")
